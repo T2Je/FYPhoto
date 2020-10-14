58e3b6c19e2226ce96dc4a41aa36f9b99a75a5f6
xiaoyang <xiaoyang@variflight.com>
2020-10-14 16:52:39 +0800

    edited url extension

--- FYPhoto/Classes/extensions/URL+mediaType.swift
+++ FYPhoto/Classes/extensions/URL+mediaType.swift
@@ -1,5 +1,5 @@
 //
-//  URL+ImageVideoChecker.swift
+//  URL+mediaType.swift
 //  FYPhoto
 //
 //  Created by xiaoyang on 2020/9/16.
@@ -11,24 +11,26 @@ import AVFoundation
 
 extension URL {
     func isImage() -> Bool {
+        let filePathURL = URL(fileURLWithPath: absoluteString)
+        let pathExtension = filePathURL.pathExtension
         guard !pathExtension.isEmpty else {
             return false
         }
         guard let uti = UTTypeCreatePreferredIdentifierForTag(kUTTagClassFilenameExtension, pathExtension as CFString, nil) else {
             return false
         }
-
         return UTTypeConformsTo(uti.takeRetainedValue(), kUTTypeImage)
     }
 
     func isVideo() -> Bool {
+        let filePathURL = URL(fileURLWithPath: absoluteString)
+        let pathExtension = filePathURL.pathExtension
         guard !pathExtension.isEmpty else {
             return false
         }
         guard let uti = UTTypeCreatePreferredIdentifierForTag(kUTTagClassFilenameExtension, pathExtension as CFString, nil) else {
             return false
         }
-        
         return UTTypeConformsTo(uti.takeRetainedValue(), kUTTypeMovie)
     }
 }
