e4064faae15d3a308fbbeb7cfab4205d3900a032
xiaoyang <xiaoyang@variflight.com>
2021-01-26 09:24:44 +0800

    Feat: added property data for SelectedImage

--- FYPhoto/Classes/SelectedModel/SelectedImage.swift
+++ FYPhoto/Classes/SelectedModel/SelectedImage.swift
@@ -17,4 +17,20 @@ public class SelectedImage {
     public let asset: PHAsset?
     public let image: UIImage
     
+    public lazy var data: Data? = {
+        var _data: Data?
+        if let asset = asset {
+            let options = PHImageRequestOptions()
+            options.deliveryMode = .highQualityFormat
+            options.isNetworkAccessAllowed = true
+            options.isSynchronous = true
+            
+            PHImageManager.default().requestImageData(for: asset, options: options) { (data, _, _, _) in
+                 _data = data
+            }
+            return _data
+        } else {
+            return image.pngData()
+        }
+    }()
 }
