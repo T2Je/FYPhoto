47118e85a1d07fedcfff8bce8ed6072a231824d5
xiaoyang <xiaoyang@variflight.com>
2021-02-08 10:04:48 +0800

    add: comments for UIViewController+Present

--- FYPhoto/Classes/Transition/UIViewController+Present.swift
+++ FYPhoto/Classes/Transition/UIViewController+Present.swift
@@ -6,37 +6,20 @@
 //
 
 import Foundation
-
-extension UIViewController: FYNameSpaceProtocol {
-    struct TransitionHolder {
-        private static var _viewControllerTransition: UIViewControllerTransitioningDelegate?
-        private static var _naviTransition: UINavigationControllerDelegate?
-        
-        static func storeViewControllerTransition(_ transition: UIViewControllerTransitioningDelegate) {
-            _viewControllerTransition = transition
-        }
-        static func storeNaviTransition(_ transition: UINavigationControllerDelegate) {
-            _naviTransition = transition
-        }
-        static func clearViewControllerTransition() {
-            _viewControllerTransition = nil
-        }
-        
-        static func clearNaviTransition() {
-            _viewControllerTransition = nil
-        }
-    }
-}
-
 extension TypeWrapperProtocol where WrappedType: UIViewController {
     
-    /// Presents a view controller with fyphoto transition.
+    /// Presents PhotoBrowser with fyphoto transition animation.
+    ///
+    /// Support three ways to display photos (if parameter `animated` is true):
+    ///  - presentingViewController and presentedViewController both comform to **PhotoTransition** protocol
+    ///  - give a closure to `transitionEssential` parameter
+    ///  - neither of the above (will be simply displayed photos in it's final location)
+    ///
     /// - Parameters:
     ///   - viewControllerToPresent: The view controller to display over the current view controller’s content.
     ///   - animated: Pass true to animate the presentation; otherwise, pass false.
     ///   - completion: The block to execute after the presentation finishes. This block has no return value and takes no parameters. You may specify nil for this parameter.
-    ///   - transitionView: A block to generate a imageView when animating. It's a alternate plan for viewController
-    ///   that do not implement PhotoTransition protocol.
+    ///   - transitionEssential: A closure to generate animation essentials with current item. Set this parameter if you don't want your presentingViewController conforms to **PhotoTransition** protocol.
     public func present(_ viewControllerToPresent: UIViewController, animated: Bool, completion: (() -> Void)?, transitionEssential: ((_ page: Int) -> PresentingVCTransitionEssential?)? = nil) {
         let transition = PhotoPresentTransitionController(viewController: viewControllerToPresent, transitionEssential: transitionEssential)
         viewControllerToPresent.modalPresentationStyle = .custom
@@ -71,12 +54,17 @@ extension TypeWrapperProtocol where WrappedType: UIViewController {
 }
 
 extension TypeWrapperProtocol where WrappedType: UINavigationController {
-    /// Pushes a view controller onto the receiver’s stack and updates the display with fyphoto transition.
+    /// Pushes PhotoBrowser onto the receiver’s stack and updates the display with fyphoto transition.
+    ///
+    /// Support three ways to display photos:
+    ///  - presentingViewController and presentedViewController both comform to **PhotoTransition** protocol
+    ///  - give a closure to `transitionEssential` parameter
+    ///  - neither of the above (will be simply displayed photos in it's final location)
+    ///
     /// - Parameters:
     ///   - viewController: The view controller to push onto the stack. This object cannot be a tab bar controller. If the view controller is already on the navigation stack, this method throws an exception.
     ///   - animated: Specify true to animate the fyphoto transition or false if you do not want the transition to be animated. You might specify false if you are setting up the navigation controller at launch time.
-    ///   - transitionView: A block to generate a imageView when animating. It's a alternate plan for viewController
-    ///   that do not implement PhotoTransition protocol.
+    ///   - transitionEssential: A closure to generate animation essentials with current item. Set this parameter if you don't want your presentingViewController conforms to **PhotoTransition** protocol.
     public func push(_ viewController: UIViewController, animated: Bool, transitionEssential: ((_ page: Int) -> PresentingVCTransitionEssential)? = nil) {
         let transition = PhotoPushTransitionController(navigationController: wrappedValue, transitionEssential: transitionEssential)
         wrappedValue.delegate = transition
@@ -99,5 +87,27 @@ extension TypeWrapperProtocol where WrappedType: UINavigationController {
         wrappedValue.delegate = transition
         wrappedValue.pushViewController(viewController, animated: true)
         UIViewController.TransitionHolder.storeNaviTransition(transition)
+    }
+}
+
+
+extension UIViewController: FYNameSpaceProtocol {
+    struct TransitionHolder {
+        private static var _viewControllerTransition: UIViewControllerTransitioningDelegate?
+        private static var _naviTransition: UINavigationControllerDelegate?
+        
+        static func storeViewControllerTransition(_ transition: UIViewControllerTransitioningDelegate) {
+            _viewControllerTransition = transition
+        }
+        static func storeNaviTransition(_ transition: UINavigationControllerDelegate) {
+            _naviTransition = transition
+        }
+        static func clearViewControllerTransition() {
+            _viewControllerTransition = nil
+        }
+        
+        static func clearNaviTransition() {
+            _viewControllerTransition = nil
+        }
     }
 }
