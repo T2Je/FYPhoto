d0ead7f4b4474ababaddfc588aeab6dac555757a
xiaoyang <xiaoyang@variflight.com>
2021-02-25 15:11:37 +0800

    fix: PhotoPicker cell hidden button can still work

--- FYPhoto/Classes/PhotoPicker/Views/SelectionButton.swift
+++ FYPhoto/Classes/PhotoPicker/Views/SelectionButton.swift
@@ -11,7 +11,11 @@ class SelectionButton: UIButton {
 
     override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {        
         if CGRect(x: -20, y: -10, width: self.frame.width + 32, height: self.frame.height + 32).contains(point) {
-            return self
+            if self.isHidden { // hidden button can still handle touch event
+                return super.hitTest(point, with: event)
+            } else {
+                return self
+            }
         }
         return super.hitTest(point, with: event)
     }
