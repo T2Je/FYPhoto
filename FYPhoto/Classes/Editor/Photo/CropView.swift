31f434c691a71177b7d0f191210117b1e016784f
xiaoyang <xiaoyang@variflight.com>
2021-04-22 11:36:08 +0800

    edit: clear code in CropView

--- FYPhoto/Classes/Editor/Photo/CropView.swift
+++ FYPhoto/Classes/Editor/Photo/CropView.swift
@@ -21,22 +21,17 @@ class CropView: UIView {
     lazy var scrollView = CropScrollView(frame: bounds)
     
     private var guideViewHasFrame = false
-//    private var cropFrameKVO: NSKeyValueObservation?
     
     init(viewModel: CropViewModel) {
         self.viewModel = viewModel
         self.imageView = ImageView(image: viewModel.image)
         super.init(frame: .zero)
-        
-//        cropFrameKVO = viewModel.observe(\.cropViewFrame, options: [.new, .old], changeHandler: { (_ , value) in
-//
-//        })
+    
         clipsToBounds = false
         addSubview(scrollView)
         scrollView.addSubview(imageView)
         addSubview(guideView)
         setupUI()
-        makeConstraints()
     }
     
     func updateViews() {
@@ -54,50 +49,8 @@ class CropView: UIView {
         imageView.center = CGPoint(x: scrollView.bounds.width/2, y: scrollView.bounds.height/2)
 
         guideView.frame = initialGuideFrame
-//        gridOverlayView.superview?.bringSubviewToFront(gridOverlayView)
     }
     
-    func makeConstraints() {
-//        scrollView.translatesAutoresizingMaskIntoConstraints = false
-//        NSLayoutConstraint.activate([
-//            scrollView.topAnchor.constraint(equalTo: topAnchor),
-//            scrollView.leadingAnchor.constraint(equalTo: leadingAnchor),
-//            scrollView.bottomAnchor.constraint(equalTo: bottomAnchor),
-//            scrollView.trailingAnchor.constraint(equalTo: trailingAnchor)
-//        ])
-                
-//        imageView.translatesAutoresizingMaskIntoConstraints = false
-//        NSLayoutConstraint.activate([
-//            imageView.leadingAnchor.constraint(greaterThanOrEqualTo: scrollView.leadingAnchor, constant: 30),
-//            imageView.trailingAnchor.constraint(lessThanOrEqualTo: scrollView.trailingAnchor, constant: -30),
-//            imageView.topAnchor.constraint(greaterThanOrEqualTo: scrollView.topAnchor, constant: 30),
-//            imageView.bottomAnchor.constraint(lessThanOrEqualTo: scrollView.bottomAnchor, constant: -30)
-//
-////            imageView.leadingAnchor.constraint(greaterThanOrEqualTo: scrollView.leadingAnchor, constant: 30),
-////            imageView.trailingAnchor.constraint(lessThanOrEqualTo: scrollView.trailingAnchor, constant: -30),
-////            imageView.topAnchor.constraint(greaterThanOrEqualTo: scrollView.topAnchor, constant: 30),
-////            imageView.bottomAnchor.constraint(lessThanOrEqualTo: scrollView.bottomAnchor, constant: -30)
-//        ])
-        
-        
-    }
-    
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        
-//        imageView.frame = viewModel.getInitialCropGuideViewRect(fromOutside: self.bounds)
-//
-////        print("imageView: \(imageView)")
-//        if !guideViewHasFrame && (imageView.frame.width < frame.width && imageView.frame.height < frame.height) {
-//            print("imageView: \(imageView)")
-//            let convertedFrame = imageView.convert(imageView.bounds, to: self)
-//            guideView.frame = convertedFrame
-//            viewModel.imageFrame = convertedFrame
-//            scrollView.contentSize = frame.size
-//            guideViewHasFrame = true
-//        }
-        
-    }
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
@@ -115,22 +68,20 @@ class CropView: UIView {
     
     func setupGuideView() {
         guideView.resizeBegan = { [weak self] in
-//            self?.touchesBegan = true
+
         }
         
         guideView.resizeEnded = { [weak self] guideViewFrame in
             guard let self = self else { return }
-            var convertedFrame = self.convert(guideViewFrame, to: self.imageView)
+            let convertedFrame = self.convert(guideViewFrame, to: self.imageView)
             
             self.scrollView.zoom(to: convertedFrame, animated: true)
             
             self.guideView.frame = GeometryHelper.getAppropriateRect(fromOutside: self.viewModel.imageFrame, inside: guideViewFrame)
-//            self?.touchesBegan = false
         }
         
         guideView.resizeCancelled = { [weak self] in
             
-//            self?.touchesBegan = false
         }
     }
     
